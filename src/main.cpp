7b2272642330dd9780144ad57eecd2dcecbac6cb
Nicolas Linden <n.linden@naarea.fr>
2024-05-23 10:39:19 +0200

    fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,32 @@
 
 #include <solver.h>
 
-// int main(int argc, char *argv[]) {
-//   (void)argc;
-//
-//   std::string inputpath(argv[1]);
-//   fmt::print("Running {:s}...\n", inputpath);
-//   Input myinput(inputpath);
-//   myinput.run();
-//
-//   return 0;
-// }
-
 int main(int argc, char *argv[]) {
   (void)argc;
-  (void)argv;
-  Chain chain("/home/nlinden/workspace/hericendre/data/chain-jeff33-long.xml");
-  DecaySolver solver;
 
-  std::map<std::string, double> initcc;
-  for (const auto &nuclide : chain.nuclides_) {
-    initcc[nuclide->name_] = 1.;
-  }
-  std::vector<double> times;
-  for (double i = -10; i <= 17; i = i + 1)
-    times.push_back(pow(10, i));
+  std::string inputpath(argv[1]);
+  fmt::print("Running {:s}...\n", inputpath);
+  Input myinput(inputpath);
+  myinput.run();
 
-  solver.run(chain, initcc, times);
-  solver.results_.to_csv("analytical_results.csv");
+  return 0;
 }
+
+// int main(int argc, char *argv[]) {
+//   (void)argc;
+//   (void)argv;
+//   Chain
+//   chain("/home/nlinden/workspace/hericendre/data/chain-jeff33-long.xml");
+//   DecaySolver solver;
+//
+//   std::map<std::string, double> initcc;
+//   for (const auto &nuclide : chain.nuclides_) {
+//     initcc[nuclide->name_] = 1.;
+//   }
+//   std::vector<double> times;
+//   for (double i = -10; i <= 17; i = i + 1)
+//     times.push_back(pow(10, i));
+//
+//   solver.run(chain, initcc, times);
+//   solver.results_.to_csv("analytical_results.csv");
+// }
