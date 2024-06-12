f67e108258ce3d78c38563cf1223b4e1456d4937
Nicolas Linden <linden.nicolas@orange.fr>
2024-06-12 19:41:37 +0200

    various

--- inc/decaysolver.h
+++ inc/decaysolver.h
@@ -4,6 +4,7 @@
 #include "results.h"
 #include <map>
 #include <string>
+#include <highfive/H5Easy.hpp>
 
 class DecaySolver {
 public:
@@ -14,6 +15,11 @@ public:
     std::vector<std::vector<double> > run(Chain &chain,
                                           const std::map<std::string, double> &ccMap,
                                           std::vector<double> times);
+
+    void to_hdf5(H5Easy::File &file) const;
+
+    Chain chain_;
+    std::map<std::string, double> ccMap_;
 
     std::map<size_t, double> Ns;
     std::map<size_t, std::map<size_t, double> > F;
