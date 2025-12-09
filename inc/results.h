2750397a96a8780f14006daa0e350327cf92e76b
Nicolas Linden <nicolas@nlinden.com>
2025-12-09 16:16:26 +0100

    refactor results

--- inc/results.h
+++ inc/results.h
@@ -1,26 +1,68 @@
 #ifndef RESULTS_HPP_INCLUDED
 #define RESULTS_HPP_INCLUDED
 #include <Eigen/Sparse>
-#include <vector>
 #include <highfive/H5Easy.hpp>
+#include <vector>
 
+/**
+ * @brief Container class for storing and exporting nuclide concentration results
+ * 
+ * This class holds the results of decay or depletion calculations, including
+ * nuclide concentrations at different time points. It provides methods to export
+ * the data to CSV and HDF5 formats.
+ */
 class Results {
 public:
-    std::vector<std::string> nuclides_;
-    std::vector<double> times_;
-    std::vector<std::vector<double> > cc_;
+  std::vector<std::vector<double>> cc_; ///< Concentration matrix [time][nuclide]
+  std::vector<std::string> nuclides_;  ///< Names of the nuclides
+  std::vector<double> times_;          ///< Time points for the results
 
-    Results();
+  /**
+   * @brief Default constructor
+   */
+  Results();
 
-    Results(const std::vector<std::vector<double> > &cc,
-            const std::vector<std::string> &nuclides, const std::vector<double> &times);
+  /**
+   * @brief Construct Results from concentration vectors
+   * 
+   * @param cc Concentration matrix as nested vectors [time][nuclide]
+   * @param nuclides Vector of nuclide names
+   * @param times Vector of time points
+   */
+  Results(const std::vector<std::vector<double>> &cc,
+          const std::vector<std::string> &nuclides,
+          const std::vector<double> &times);
 
-    Results(const std::vector<Eigen::VectorXd> &cc, const std::vector<std::string> &nuclides,
-            const std::vector<double> &times);
+  /**
+   * @brief Construct Results from Eigen vectors
+   * 
+   * @param cc Vector of Eigen concentration vectors, one per time point
+   * @param nuclides Vector of nuclide names
+   * @param times Vector of time points
+   */
+  Results(const std::vector<Eigen::VectorXd> &cc,
+          const std::vector<std::string> &nuclides,
+          const std::vector<double> &times);
 
-    void to_csv(const std::string &path, bool ignore_zeros = true) const;
+  /**
+   * @brief Export results to CSV file
+   * 
+   * Writes the concentration data to a CSV file with nuclides as columns
+   * and time points as rows.
+   * 
+   * @param path Output file path
+   * @param ignore_zeros If true, omit nuclides that have zero concentration at all times
+   */
+  void to_csv(const std::string &path, bool ignore_zeros = true) const;
 
-    void to_hdf5(H5Easy::File &file) const;
+  /**
+   * @brief Export results to HDF5 file
+   * 
+   * Writes the concentration data, nuclide names, and time points to an HDF5 file.
+   * 
+   * @param file Open HDF5 file object to write to
+   */
+  void to_hdf5(H5Easy::File &file) const;
 };
 
 #endif
--- src/results.cpp
+++ src/results.cpp
@@ -5,55 +5,58 @@
 Results::Results() = default;
 
 Results::Results(const std::vector<std::vector<double>> &cc,
-                 const std::vector<std::string> &nuclides, const std::vector<double> &times)
-{
-    cc_ = cc;
-    nuclides_ = nuclides;
-    times_ = times;
-};
+                 const std::vector<std::string> &nuclides,
+                 const std::vector<double> &times)
+    : cc_(cc), nuclides_(nuclides), times_(times){}
 
 Results::Results(const std::vector<Eigen::VectorXd> &cc,
-                 const std::vector<std::string> &nuclides, const std::vector<double> &times)
-{
-    for (auto &vec : cc)
-    {
-        cc_.emplace_back(vec.data(), vec.data() + vec.size());
+                 const std::vector<std::string> &nuclides,
+                 const std::vector<double> &times): nuclides_(nuclides), times_(times) {
+  cc_.reserve(cc.size());
+  for (const auto &vec : cc) {
+    cc_.emplace_back(vec.data(), vec.data() + vec.size());
+  }
+}
+
+void Results::to_csv(const std::string &path, bool ignore_zeros) const {
+  auto out = fmt::output_file(path);
+
+  // HEADER
+  out.print("nuclide");
+  for (const auto &t : times_) {
+    out.print(",{}", t);
+  }
+  out.print("\n");
+
+  // CONTENT
+  for (size_t inuc = 0; inuc < nuclides_.size(); inuc++) {
+    bool has_nonzero = false;
+    
+    // Check if this nuclide has any non-zero values
+    if (ignore_zeros) {
+      for (const auto &concentrations : cc_) {
+        if (concentrations[inuc] != 0.0) {
+          has_nonzero = true;
+          break;
+        }
+      }
+      if (!has_nonzero) {
+        continue;
+      }
     }
-    nuclides_ = nuclides;
-    times_ = times;
-};
-
-void Results::to_csv(const std::string &path, const bool ignore_zeros) const
-{
-    auto out = fmt::output_file(path);
-
-    // HEADER
-    for (const auto &t : this->times_)
-        out.print(",{}", t);
+    
+    // Write the row
+    out.print("{}", nuclides_[inuc]);
+    for (const auto &concentrations : cc_) {
+      out.print(",{:e}", concentrations[inuc]);
+    }
     out.print("\n");
+  }
+}
 
-    // CONTENT
-    for (size_t inuc = 0; inuc < this->nuclides_.size(); inuc++)
-    {
-        std::string line = fmt::format("{:s}", this->nuclides_[inuc]);
-        bool zero_flag = true;
-        for (const auto &concentrations : this->cc_)
-        {
-            line += fmt::format(",{:e}", concentrations[inuc]);
-            if (concentrations[inuc] != 0.)
-                zero_flag = false;
-        }
-        line += fmt::format("\n");
-        if (!zero_flag || !ignore_zeros)
-        {
-            out.print("{}", line);
-        }
-    }
-};
 
-void Results::to_hdf5(H5Easy::File &file) const
-{
-    H5Easy::dump(file, "CONCENTRATIONS", this->cc_);
-    H5Easy::dump(file, "TIMES", this->times_);
-    H5Easy::dump(file, "NUCLIDES", this->nuclides_);
+void Results::to_hdf5(H5Easy::File &file) const {
+  H5Easy::dump(file, "CONCENTRATIONS", this->cc_);
+  H5Easy::dump(file, "TIMES", this->times_);
+  H5Easy::dump(file, "NUCLIDES", this->nuclides_);
 }
